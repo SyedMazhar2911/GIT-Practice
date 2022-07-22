1b6624f22d99dcb9a3b56b6a217d352076dc3366
Syed <SyedMazhar2911@gmail.com>
2022-07-22 03:54:35 +0300

    devtest class Commit  new line added

--- src/main/java/DevTestclass.java
+++ src/main/java/DevTestclass.java
@@ -3,6 +3,7 @@ public class DevTestclass {
     public static void main(String[] args) {
         System.out.println("DevTestClass");
         //New line is added
+        //Second line is added
 
     }
 
